07bd9b5a0e05ef95809344786644b0c5f719e374
idkwhatusernameuse <idkwhatusernameuse@gmail.com>
2020-06-04 20:28:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# idkwhatusernameuse.github.io
+
+This is my personal website. Feel free to fork it and modify it for your own website.
+
+### License
+
+MIT
---
+++ js/transitions.js
@@ -0,0 +1,34 @@
+import Highway from '@dogstudio/highway'
+import {TimelineLite} from 'gsap'
+
+class Fade extends Highway.Transition {
+    in ({from, to, done}) {
+        const tl = new TimelineLite();
+        tl.fromTo(to, 0.7,
+            { left: '100%' },
+            { 
+                left: '0%',
+                onComplete: function() { 
+                    from.remove()
+                    done() 
+                }
+            }
+        )
+    }
+    out ({from, done}) {
+        const tl = new TimelineLite();
+        tl.fromTo(from, 0.7,
+            { left: '0%' },
+            {
+                left: '-100%',
+                onComplete: function() {
+                    
+                    done()
+                }
+            }
+        )
+        //done()
+    }
+}
+
+export default Fade;
