d68144a9c137c4e9dfcd147fb2ce034382481daa
idkwuu <idkwhatusernameuse@gmail.com>
2020-09-06 19:43:24 -0600

    Update more links and rebuild website

--- src/pages/Links.svelte
+++ src/pages/Links.svelte
@@ -18,7 +18,7 @@
   <Link
     title="Telegram"
     src="images/telegram.svg"
-    link="https://t.me/idkwhatusernameuse" />
+    link="https://t.me/idkwuu" />
   <Link
     title="GitHub"
     src="images/github.png"
@@ -28,18 +28,14 @@
     src="images/reddit.svg"
     link="https://reddit.com/u/idkwhatusernameuse" />
   <Link
-    title="Spotify"
-    src="images/spotify.svg"
-    link="https://open.spotify.com/user/0oqievwv1qtt4n186ulks562n?si=aeZ09LwwRbSvAw2oSxuOSA" />
-  <Link
     title="YouTube"
     src="images/youtube.svg"
     link="https://www.youtube.com/channel/UCIOQMvPHLcBT7-x2Qx9NW8w" />
   <Link
     title="Steam"
     src="images/steam.svg"
-    link="https://steamcommunity.com/id/idkwhatusernameuse" />
+    link="https://steamcommunity.com/id/idkwuu" />
     
   <p>Mastodon: @idkwhatusernameuse@mastodon.online</p>
-  <p>Discord: idkwhatusernameuse#7097</p>
+  <p>Discord: idkwuu#7097</p>
 </div>
